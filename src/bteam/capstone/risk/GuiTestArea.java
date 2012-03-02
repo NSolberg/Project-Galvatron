da7e3f83df742d407a08b38b2aa1d48ab346b7fc
Ian Paterson <ian.paterson.08@cnu.edu>
2012-03-01 19:08:45 -0500

    Added gui test shit

---
+++ src/bteam/capstone/risk/GuiTestArea.java
@@ -0,0 +1,14 @@
+package bteam.capstone.risk;
+
+public class GuiTestArea {
+
+	/**
+	 * Le sigh. Lots of work to do here
+	 * 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
